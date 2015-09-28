89bfc30cdc4d79a4e3e9eb0c5b0855e0bb1a8183
runongirlrunon <runongirlrunon@gmail.com>
2015-09-27 17:34:18 -0700

    Initial Commit

---
+++ Playlist-LinkedList/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Playlist-LinkedList
+//
+//  Created by Lindsey Hogg on 9/27/15.
+//  Copyright © 2015 Lindsey Hogg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
